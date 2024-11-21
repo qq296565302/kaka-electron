e246002f236e3cd8137f5c058038801d1e9b5f4a
Zhaoshijun <13165445560@163.com>
2024-11-21 16:55:49 +0800

    增加了热更新工具，支持自动检测文件变化并刷新应用

--- main.js
+++ main.js
@@ -2,10 +2,21 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('node:path')
 const ipcHandler = require('./ipcHandler')
 
+/**
+ * 热更新工具，支持自动检测文件变化并刷新应用
+ * 安装：npm install electron-reload --save-dev
+ * __dirname：监听项目目录下的文件。
+ * electron：指定 Electron 可执行文件路径。
+ */
+
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 1024,
-    height: 576,
+    width: 555,
+    height: 855,
     frame: false,
     opacity: 1,
     resizable: false, // 禁止改变主窗口尺寸
