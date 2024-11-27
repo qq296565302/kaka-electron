a3b9b0ed4d1f5976ef97619cccab109b7ee37a6b
Zhaoshijun <13165445560@163.com>
2024-11-27 15:51:14 +0800

    优化财联社数据展示

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="Page-Home _drag">
 		<!-- CLS 数据 -->
-		<div class="cls-container">
+		<div class="cls-container" ref="clsContainer" @scroll="handleClsScroll">
 			<p class="lastUpdated PingFang _drag">
 				<span>最后更新时间：{{ data.lastUpdated }}</span>
 				<span class="update _no-drag" @click="requestCollection.getClsData()">手动更新</span>
@@ -9,6 +9,11 @@
 			<div class="cls-item PingFang _no-drag" v-for="item in data.cls" :key="item['发布时间']">
 				<div class="cls-item-title" v-if="item['标题']">{{ item['标题'] }}</div>
 				<div class="cls-item-content">{{ item['内容'] }}</div>
+				<p class="posters">生成分享海报</p>
+			</div>
+			<!-- 回到顶部 -->
+			<div class="back-to-top" @click="handleBackToTop" v-if="clsBackToTop">
+				<el-icon class="icon"><Top /></el-icon>
 			</div>
 		</div>
 	</div>
@@ -57,7 +62,21 @@ const data = reactive({
 	lastUpdated: '', // 最后更新时间
 	cls: [],
 })
-
+// CLS 滚动事件 如果滑动距离大于 500px 显示回到顶部按钮，点击按钮，返回顶部
+const clsContainer = ref(null)
+const clsBackToTop = ref(false)
+const handleClsScroll = () => {
+	if (clsContainer.value) {
+		const scrollTop = clsContainer.value.scrollTop // 获取滚动距离
+		clsBackToTop.value = scrollTop > 500
+	}
+}
+const handleBackToTop = async () => {
+	if (clsContainer.value) {
+		clsContainer.value.scrollTop = 0 // 设置滚动距离为 0
+		await requestCollection.getClsData()
+	}
+}
 onBeforeMount(async () => {
 	await requestCollection.getClsData()
 })
@@ -72,6 +91,7 @@ defineExpose({
 	height: 100%;
 	overflow: auto;
 	padding: 30px;
+	position: relative;
 	width: 100%;
 	.lastUpdated {
 		color: #919191;
@@ -110,6 +130,30 @@ defineExpose({
 			text-indent: 2em;
 			text-align: justify;
 			padding: 20px 0;
+		}
+		.posters {
+			cursor: pointer;
+			font-size: 14px;
+			text-align: right;
+		}
+	}
+	.back-to-top {
+		background-color: rgba(255, 255, 255, 0.6);
+		border-radius: 100%;
+		bottom: 20px;
+		cursor: pointer;
+		color: rgba(0, 6, 32, 1);
+		font-size: 32px;
+		height: 50px;
+		line-height: 50px;
+		position: fixed;
+		right: 20px;
+		text-align: center;
+		width: 50px;
+		.icon {
+			left: 0px;
+			position: relative;
+			top: 5px;
 		}
 	}
 }
