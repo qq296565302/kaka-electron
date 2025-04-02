a5dabccc3da2cad32585ae0fc58d073aa307d2db
Zhaoshijun <13165445560@163.com>
2025-04-02 17:02:06 +0800

    移除代码

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,83 +1,16 @@
 <template>
-	<div class="Page-Home">
-		<div class="container">
-			<Cls v-if="data.tabIndex === 0" :key="data.tabIndex"/>
-			<CapitalMarket v-if="data.tabIndex === 1" :key="data.tabIndex"/>
-		</div>
-		<div class="navBar _no-drag">
-			<cmpsvg :use="svgIcon.BN_information" class="nav-item" :class="{ active: data.tabIndex === 0 }" @click="data.tabIndex = 0" />
-			<cmpsvg :use="svgIcon.BN_stock" class="nav-item" :class="{ active: data.tabIndex === 1 }" @click="data.tabIndex = 1" />
-		</div>
-	</div>
+  <div class='Page-Home'></div>
 </template>
 
 <script setup>
-import { ref, reactive, toRefs, onBeforeMount, onMounted, watch, computed, getCurrentInstance, h, nextTick } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-
-import Cls from './Information/cls.vue'
-import CapitalMarket from './CapitalMarket/index.vue'
-
-import crud from '@/utils/crud'
-import API_Service from 'kaka-api-service'
-
-const proxy = getCurrentInstance()?.proxy
-const { request, storage, svgIcon } = proxy
-const pageName = 'home'
-
-API_Service.registerApi(pageName, {
-	fetch: {
-		default: (data) => request.get(`/finance/realTimeQuotes`, data),
-	},
+const { } = getCurrentInstance()?.proxy;
+onBeforeMount(() => {
 })
-
-/**
- * 请求集合
- */
-import dayjs from 'dayjs'
-const requestCollection = {
-	// 获取实时数据
-	getRealData: async () => {
-		const result = await crud.launch(() => {
-			return API_Service.fetch(pageName, {
-				symbol: '300444',
-			})
-		})
-		console.log({ result })
-	},
-}
-const data = reactive({
-	tabIndex: 0,
+onMounted(() => {
 })
-
-onMounted(() => {})
 defineExpose({
-	...toRefs(data),
 })
+
 </script>
-<style scoped lang="scss">
-.Page-Home {
-	.container {
-		height: calc(100% - 80px);
-		width: 100%;
-	}
-	.navBar {
-		align-items: center;
-		background-color: rgb(23, 46, 73);
-		display: flex;
-		height: 80px;
-		justify-content: space-around;
-		position: relative;
-		width: 100%;
-		z-index: 5;
-		.nav-item {
-			height: 36px;
-			width: 36px !important;
-			filter: grayscale(80);
-			&.active {
-				filter: grayscale(0);
-			}
-		}
-	}
-}
+<style scoped lang='scss'>
 </style>
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
-// vite.config.js
 import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
 import { dirname, resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import { fileURLToPath } from 'url';
 
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default ({ mode }) => {
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
@@ -11,6 +12,9 @@ export default ({ mode }) => {
   return defineConfig({
     plugins: [
       vue(),
+      AutoImport({  
+        imports: ["vue", "vue-router", "pinia"],
+      }),
     ],
     resolve: {
       alias: {
